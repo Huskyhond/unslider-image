d684da87f0c6065f347066c508f1626a0ceff0a4
pashabd <pashabd@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-05-27 15:22:12 +0000

    unnecessary link content remove
    git-svn-id: https://plugins.svn.wordpress.org/unslider-image/trunk@921664 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/active.js
+++ js/active.js
@@ -9,28 +9,7 @@ jQuery(document).ready(function(){
 				speed: 1000
 			});
 		
-			//  Find any element starting with a # in the URL
-			//  And listen to any click events it fires
-			jQuery('a[href^="#"]').click(function() {
-				//  Find the target element
-				var target = jQuery(jQuery(this).attr('href'));
-				
-				//  And get its position
-				var pos = target.offset(); // fallback to scrolling to top || {left: 0, top: 0};
-				
-				//  jQuery will return false if there's no element
-				//  and your code will throw errors if it tries to do .offset().left;
-				if(pos) {
-					//  Scroll the page
-					jQuery('html, body').animate({
-						scrollTop: pos.top,
-						scrollLeft: pos.left
-					}, 1000);
-				}
-				
-				//  Don't let them visit the url, we'll scroll you there
-				return false;
-			});
+			
 			
 			
 
